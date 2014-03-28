196338df3f4b66ac6b84e5638ece87b1ca8b8d07
mzabara <mzabara@gmail.com>
2014-03-28 21:44:35 +0700

    Главная страница c новыи слайдером

--- js/function.js
+++ js/function.js
@@ -1,7 +1,31 @@
-(function (cash) {
     $(document).ready(function () {
-        $('#catalog-multilevel-menu, #catalog-multilevel-menu-2, #catalog-multilevel-menu-3').on('show.bs.collapse', function () {
-            $('.catalog-multilevel-menu .in, .collapse .in').collapse('hide');
+
+
+        $('[data-toggle="popover"]').popover();
+
+        $('body').on('click', function (e) {
+            $('[data-toggle="popover"]').each(function () {
+                if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+                    $(this).popover('hide');
+                }
+            });
         });
+
+        $(".close-compare-collapse").on("click", function() {
+            $('#compare-collapse').collapse('hide');
+        });
+
+        $(".close-favorite-collapse").on("click", function() {
+            $('#favorite-collapse').collapse('hide');
+        });
+
+        $(".close-shopping-cart").on("click", function() {
+            $('#shopping-cart-collapse').collapse('hide');
+        });
+
+//        $('#catalog-multilevel-menu, #catalog-multilevel-menu-2, #catalog-multilevel-menu-3').on('show.bs.collapse', function () {
+//            $('.catalog-multilevel-menu .in').collapse('hide');
+//        });  $('#catalog-multilevel-menu, #catalog-multilevel-menu-2, #catalog-multilevel-menu-3').on('show.bs.collapse', function () {
+//            $('.catalog-multilevel-menu .in').collapse('hide');
+//        });
     });
-}(jQuery));
